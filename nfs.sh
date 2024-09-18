536e3f0bef17f930abe831612867e8a4513427ec
cpouthier <80251456+cpouthier@users.noreply.github.com>
2024-09-18 14:01:12 +0200

    commit

--- nfs.sh
+++ nfs.sh
@@ -1,32 +1,50 @@
-#Install NFS
-apt update
-apt install nfs-kernel-server
 
-#Create and configure directory to share
+
+## Install NFS server
+
+
+apt update
+apt install nfs-kernel-server -y
+
+
+## Create and configure directory to share
+
+
 mkdir -p /srv/nfs_share
 chown nobody:nogroup /srv/nfs_share
 chmod 755 /srv/nfs_share
 
-#Configure exports
-#Example:
-#/srv/nfs_share    192.168.1.0/24(rw,sync,no_subtree_check)
-#/srv/nfs_share    127.0.0.1/24(rw,sync,no_subtree_check)
+
+## Configure exports
+
+
 echo "/srv/nfs_share    127.0.0.1/24(rw,sync,no_subtree_check)" | tee -a /etc/exports
 
-#Apply modification and restart service
+
+## Apply modification and restart service
+
+
 exportfs -a
 systemctl restart nfs-kernel-server
 
-#Mount share on client
+
+## Mount share on client
+
+
 apt install nfs-common
 mkdir -p /mnt/nfs_share
 mount 127.0.0.1:/srv/nfs_share /mnt/nfs_share
 
-#Automate Mount in /etc/fstab
-# Example: 192.168.1.10:/srv/nfs_share /mnt/nfs_share nfs defaults 0 0
+
+## Automate mount in /etc/fstab
+
+
 echo "127.0.0.1:/srv/nfs_share /mnt/nfs_share nfs defaults 0 0" | tee -a /etc/fstab
 
-#Create a PV defining the exported NFS share
+
+## Create a PV on the exported NFS share
+
+
 echo | kubectl apply -f - << EOF
 apiVersion: v1
 kind: PersistentVolume
@@ -48,7 +66,10 @@ spec:
       server: 127.0.0.1
 EOF
 
-#Create PVC
+
+## Create the NFS PVC
+
+
 echo | kubectl apply -f - << EOF
 apiVersion: v1
 kind: PersistentVolumeClaim
@@ -64,7 +85,10 @@ spec:
          storage: 10Gi
 EOF
 
+## Create the NFS location profile for Veeam Kasten
 
+
+echo | kubectl apply -f - << EOF
 kind: Profile
 apiVersion: config.kio.kasten.io/v1alpha1
 metadata:
@@ -75,7 +99,7 @@ spec:
     type: FileStore
     fileStore:
       claimName: nfs-pvc
-      path: /
+      path: /srv/nfs_share
     credential:
       secretType: ""
       secret:
@@ -84,4 +108,6 @@ spec:
         name: ""
         namespace: ""
   type: Location
+EOF
+
 
